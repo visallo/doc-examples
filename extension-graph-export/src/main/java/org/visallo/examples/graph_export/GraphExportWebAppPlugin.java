d3e4a28e55afd47ad1dfd0907f69b84beccb1961
Jason Harwig <jason@pinepointsoftware.com>
2017-02-23 17:04:54 -0600

    graph export

---
+++ extension-graph-export/src/main/java/org/visallo/examples/graph_export/GraphExportWebAppPlugin.java
@@ -0,0 +1,24 @@
+package org.visallo.examples.graph_export;
+
+import com.v5analytics.webster.Handler;
+import org.visallo.core.model.Description;
+import org.visallo.core.model.Name;
+import org.visallo.web.WebApp;
+import org.visallo.web.WebAppPlugin;
+
+import javax.servlet.ServletContext;
+
+@Name("Graph Export")
+@Description("Add menu options to export graph / workspace")
+public class GraphExportWebAppPlugin implements WebAppPlugin {
+
+    @Override
+    public void init(WebApp app, ServletContext servletContext, Handler authenticationHandler) {
+        app.registerJavaScript("/org/visallo/examples/graph_export/plugin.js", true);
+        app.registerJavaScript("/org/visallo/examples/graph_export/configuration.js", false);
+        app.registerJavaScriptTemplate("/org/visallo/examples/graph_export/template.hbs");
+        app.registerLess("/org/visallo/examples/graph_export/style.less");
+        app.registerResourceBundle("/org/visallo/examples/graph_export/messages.properties");
+    }
+
+}
---
+++ extension-graph-export/src/main/resources/org/visallo/examples/graph_export/configuration.js
@@ -0,0 +1,22 @@
+define([
+    'public/v1/api',
+    'hbs!./template'
+], function(
+    api,
+    template) {
+    'use strict';
+
+    return api.defineComponent(GraphExportExample);
+
+    function GraphExportExample() {
+
+        this.after('initialize', function() {
+            this.$node.html(template({
+                productId: this.attr.productId,
+                workspaceId: this.attr.workspaceId
+            }));
+            this.$node.find('pre')[0].textContent = JSON.stringify(this.attr.cy.json(), null, 2)
+        });
+
+    }
+});
