1ea91e17e56ae6a8e00d53c87b83d77d3d03c54b
Jason Harwig <jason@pinepointsoftware.com>
2017-02-23 11:44:39 -0600

    dashboard layout

---
+++ extension-dashboard-layout/src/main/java/org/visallo/examples/dashboard_layout/DashboardLayoutWebAppPlugin.java
@@ -0,0 +1,20 @@
+package org.visallo.examples.dashboard_layout;
+
+import com.v5analytics.webster.Handler;
+import org.visallo.core.model.Description;
+import org.visallo.core.model.Name;
+import org.visallo.web.WebApp;
+import org.visallo.web.WebAppPlugin;
+
+import javax.servlet.ServletContext;
+
+@Name("Dashboard Layout")
+@Description("Define dashboard layout for new cases")
+public class DashboardLayoutWebAppPlugin implements WebAppPlugin {
+
+    @Override
+    public void init(WebApp app, ServletContext servletContext, Handler authenticationHandler) {
+        app.registerJavaScript("/org/visallo/examples/dashboard_layout/plugin.js", true);
+    }
+
+}
