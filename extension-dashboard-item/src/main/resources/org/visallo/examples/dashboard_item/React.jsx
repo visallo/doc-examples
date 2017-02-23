d47301aeb0f018cc72d94e475902837cb2cf039e
Jason Harwig <jason@pinepointsoftware.com>
2017-02-23 11:44:30 -0600

    Add refreshData

--- extension-dashboard-item/src/main/resources/org/visallo/examples/dashboard_item/React.jsx
+++ extension-dashboard-item/src/main/resources/org/visallo/examples/dashboard_item/React.jsx
@@ -4,11 +4,23 @@ define([
     'use strict';
 
     const ReactDashboardItem = React.createClass({
+        componentDidMount() {
+            const { item, extension } = this.props;
+            $(this.refs.wrap.parentNode).on('refreshData', (event) => {
+                this.props.configurationChanged({
+                    item: { ...item, configuration: { ...item.configuration, count: 0 } },
+                    extension
+                })
+            });
+        },
+        componentWillUnmount() {
+            $(this.refs.wrap.parentNode).off('refreshData');
+        },
         render() {
             const { item, extension } = this.props;
             const { configuration: { count } } = item;
             return (
-                <div>
+                <div ref="wrap">
                     Hello World
                     Count is {count || 'Not Set'}
                 </div>
