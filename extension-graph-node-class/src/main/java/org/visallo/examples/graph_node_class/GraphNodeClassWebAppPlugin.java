35c386a4a9eb5ad3b67933f63f299985ae1ac104
Jason Harwig <jason@pinepointsoftware.com>
2017-02-23 20:31:38 -0600

    graph node class

---
+++ extension-graph-node-class/src/main/java/org/visallo/examples/graph_node_class/GraphNodeClassWebAppPlugin.java
@@ -0,0 +1,21 @@
+package org.visallo.examples.graph_node_class;
+
+import com.v5analytics.webster.Handler;
+import org.visallo.core.model.Description;
+import org.visallo.core.model.Name;
+import org.visallo.web.WebApp;
+import org.visallo.web.WebAppPlugin;
+
+import javax.servlet.ServletContext;
+
+@Name("Graph Node Class")
+@Description("Function that can change cytoscape classes of nodes")
+public class GraphNodeClassWebAppPlugin implements WebAppPlugin {
+
+    @Override
+    public void init(WebApp app, ServletContext servletContext, Handler authenticationHandler) {
+        app.registerJavaScript("/org/visallo/examples/graph_node_class/plugin.js", true);
+        app.registerResourceBundle("/org/visallo/examples/graph_node_class/messages.properties");
+    }
+
+}
