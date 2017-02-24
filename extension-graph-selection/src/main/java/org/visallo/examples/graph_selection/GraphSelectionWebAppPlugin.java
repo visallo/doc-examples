ba76f66a8b2441da0c5bbaf188357473d69112da
Jason Harwig <jason@pinepointsoftware.com>
2017-02-24 15:19:17 -0600

    Graph selection:

---
+++ extension-graph-selection/src/main/java/org/visallo/examples/graph_selection/GraphSelectionWebAppPlugin.java
@@ -0,0 +1,21 @@
+package org.visallo.examples.graph_selection;
+
+import com.v5analytics.webster.Handler;
+import org.visallo.core.model.Description;
+import org.visallo.core.model.Name;
+import org.visallo.web.WebApp;
+import org.visallo.web.WebAppPlugin;
+
+import javax.servlet.ServletContext;
+
+@Name("Graph Selection")
+@Description("Add custom graph selection menu items")
+public class GraphSelectionWebAppPlugin implements WebAppPlugin {
+
+    @Override
+    public void init(WebApp app, ServletContext servletContext, Handler authenticationHandler) {
+        app.registerJavaScript("/org/visallo/examples/graph_selection/plugin.js", true);
+        app.registerResourceBundle("/org/visallo/examples/graph_selection/messages.properties");
+    }
+
+}
