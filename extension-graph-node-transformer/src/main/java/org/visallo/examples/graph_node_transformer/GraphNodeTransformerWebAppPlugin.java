551d4b7f837763dbcafa7e181f7fdf423a3de28b
Jason Harwig <jason@pinepointsoftware.com>
2017-02-24 10:14:50 -0600

    graph node transformer

---
+++ extension-graph-node-transformer/src/main/java/org/visallo/examples/graph_node_transformer/GraphNodeTransformerWebAppPlugin.java
@@ -0,0 +1,20 @@
+package org.visallo.examples.graph_node_transformer;
+
+import com.v5analytics.webster.Handler;
+import org.visallo.core.model.Description;
+import org.visallo.core.model.Name;
+import org.visallo.web.WebApp;
+import org.visallo.web.WebAppPlugin;
+
+import javax.servlet.ServletContext;
+
+@Name("Graph Node Transformer")
+@Description("Allows extensions to adjust the data attribute of cytoscape nodes.")
+public class GraphNodeTransformerWebAppPlugin implements WebAppPlugin {
+
+    @Override
+    public void init(WebApp app, ServletContext servletContext, Handler authenticationHandler) {
+        app.registerJavaScript("/org/visallo/examples/graph_node_transformer/plugin.js", true);
+    }
+
+}
