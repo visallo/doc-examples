1155d95c81da4ae6d21c97e47df312fcb9a9f0b0
Jason Harwig <jason@pinepointsoftware.com>
2017-02-26 13:43:49 -0600

    logout

---
+++ extension-logout/src/main/resources/org/visallo/examples/logout/plugin.js
@@ -0,0 +1,18 @@
+require(['public/v1/api'], function(visallo) {
+
+    visallo.registry.registerExtension('org.visallo.logout', function() {
+        var seconds = 3;
+        alert('Will logout in ' + seconds + ' seconds');
+        _.delay(function() {
+            visallo.connect()
+                .then(function(connected) {
+                    return connected.dataRequest('user', 'logout');
+                })
+                .then(function() {
+                    window.location.reload();
+                })
+        }, seconds * 1000)
+        return false;
+    });
+
+});
