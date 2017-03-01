748635131933081c08b531a7671417962e1c470f
Jason Harwig <jason.harwig@gmail.com>
2017-03-01 13:38:07 -0600

    Update plugin.js

--- extension-entity-listitemrenderer/src/main/resources/org/visallo/examples/entity_listitemrenderer/plugin.js
+++ extension-entity-listitemrenderer/src/main/resources/org/visallo/examples/entity_listitemrenderer/plugin.js
@@ -2,7 +2,7 @@ require(['public/v1/api'], function(visallo) {
 
     visallo.registry.registerExtension('org.visallo.entity.listItemRenderer', {
         canHandle: function(element, usageContext) {
-            return true;
+            return usageContext === 'searchresults';
         },
         componentPath: 'org/visallo/examples/entity_listitemrenderer/component'
     });
