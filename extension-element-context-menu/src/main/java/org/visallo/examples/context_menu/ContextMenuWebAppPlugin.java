bab1856ac6b69e5edfaa08ac253fb5d7fdb22777
Joey Burke <joeybrk372@users.noreply.github.com>
2017-07-21 16:48:07 -0400

    Add element context menu example (#3)
    Add element context menu example

---
+++ extension-element-context-menu/src/main/java/org/visallo/examples/context_menu/ContextMenuWebAppPlugin.java
@@ -0,0 +1,22 @@
+package org.visallo.examples.context_menu;
+
+import com.v5analytics.webster.Handler;
+import org.visallo.core.model.Description;
+import org.visallo.core.model.Name;
+import org.visallo.web.WebApp;
+import org.visallo.web.WebAppPlugin;
+
+import javax.servlet.ServletContext;
+
+@Name("Element Context Menu")
+@Description("Add options to element right click menus")
+public class ContextMenuWebAppPlugin implements WebAppPlugin {
+
+    @Override
+    public void init(WebApp app, ServletContext servletContext, Handler authenticationHandler) {
+        app.registerJavaScript("/org/visallo/examples/context_menu/plugin.js", true);
+        app.registerResourceBundle("/org/visallo/examples/context_menu/messages.properties");
+        app.registerLess("/org/visallo/examples/context_menu/style.less");
+    }
+
+}
